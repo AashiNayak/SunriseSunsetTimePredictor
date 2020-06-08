e795c89ec1d332044655ec89c4ac754d52514d45
AashiNayak <lostgirl0108@gmail.com>
2020-06-08 15:56:49 +0530

    Add project files.

---
+++ SunriseSunsetTimePredictor/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SunriseSunsetTimePredictor
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            ApiConnection.Initialize();
+        }
+
+        private async void GetDataBtn_Click(object sender, RoutedEventArgs e)
+        {
+            if (latText.Text == "" || lngText.Text == "")
+            {
+                MessageBox.Show("Enter Latitude and Longitude");
+            }
+            else if (latText.Text == "0" || lngText.Text == "0")
+            {
+                MessageBox.Show("Invalid Request");
+            }
+            else
+            {
+                float latitude = float.Parse(latText.Text);
+                float longitude = float.Parse(lngText.Text);
+
+                var predict = await TimePredictor.LoadTime(latitude, longitude);
+
+                riseLabel.Content = $"Sunrise is at {predict.Sunrise.ToLocalTime().ToShortTimeString()}";
+                setLabel.Content = $"Sunset is at {predict.Sunset.ToLocalTime().ToShortTimeString()}";
+
+            }
+        }
+    }
+}
---
+++ SunriseSunsetTimePredictor/TimePredictor.cs
@@ -0,0 +1,31 @@
+﻿using SunriseSunsetTimePredictor.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SunriseSunsetTimePredictor
+{
+    class TimePredictor
+    {
+        public static async Task<DataModel> LoadTime(float lat, float lng)
+        {
+            string path = $"https://api.sunrise-sunset.org/json?lat={ lat }&lng={ lng }";
+
+            using (HttpResponseMessage response = await ApiConnection.ApiClient.GetAsync(path))
+            {
+                if (response.IsSuccessStatusCode)
+                {
+                    ResultModel time = await response.Content.ReadAsAsync<ResultModel>();
+                    return time.Results;
+                }
+                else
+                {
+                    throw new Exception(response.ReasonPhrase);
+                }
+            }
+        }
+    }
+}
